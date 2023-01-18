eca5c5442725b44ba826b9adf0a809fbbc99f284
elizabethnag1311 <109186428+elizabethnag1311@users.noreply.github.com>
2023-01-18 11:16:45 -0300

    Formularios Reactivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveComponent } from './pages/reactive/reactive.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReactiveComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/reactive/reactive.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { from } from 'rxjs';
+
+@Component({
+  selector: 'app-reactive',
+  templateUrl: './reactive.component.html',
+  styleUrls: ['./reactive.component.css']
+})
+export class ReactiveComponent implements OnInit {
+
+  forma!: FormGroup;
+
+  constructor( private fb: FormBuilder) {
+    this.crearFormulario()
+   }
+
+  ngOnInit(): void {
+  }
+
+  get pasatiempos() {
+    return this.forma.get('pasatiempos') as FormArray;
+  }
+
+  inputNoValido( input: string ){
+   return this.forma.get( input )?.invalid && this.forma.get( input )?.touched
+  }
+
+  provincia( input: string ) {
+    return this.forma.get(`${input}.provincia`)?.invalid && this.forma.get(`${input}.provincia`)?.touched
+  }
+
+  
+  ciudad( input: string ) {
+    return this.forma.get(`${input}.ciudad`)?.invalid && this.forma.get(`${input}.ciudad`)?.touched
+  }
+  
+
+  //sintaxis del objeto propiedad: ['valor por defecto','validadores sincronos',validadores asincronos]
+                                         // sintaxis [validadores sincronos con 2 o mas validaciones , ...] 
+  crearFormulario() {
+    this.forma = this.fb.group({
+      nombre  : ['',   Validators.required],
+      apellido: ['',   Validators.required],
+      correo  : ['', [ Validators.required, Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.com$') ] ],
+      //group aninado en el formulario
+      direccion: this.fb.group({
+        provincia  : ['', Validators.required],
+        ciudad     : ['', Validators.required]
+      }),
+      //array dentro del formulario
+      pasatiempos: this.fb.array([
+
+      ])
+    }) 
+  }
+
+  agregarPasatiempo(){
+    this.pasatiempos.push( this.fb.control(['']) );
+  }
+  borrarPasatiempo( i: number ){
+    this.pasatiempos.removeAt(i);
+  }
+
+  guardar() {
+    console.log(this.forma)
+  if ( this.forma.invalid) {
+    this.forma.markAllAsTouched();
+ }else this.forma.reset();
+}
+
+
+}
