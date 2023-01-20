6bc7becd8277b33e1ebe658de50ae26236fd1cb2
elizabethnag1311 <109186428+elizabethnag1311@users.noreply.github.com>
2023-01-20 18:17:55 -0300

    Formularios Reactivos

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { from } from 'rxjs';
+import { __values } from 'tslib';
 import { ValidadoresService } from '../../services/validadores.service';
 
 @Component({
@@ -10,12 +11,13 @@ import { ValidadoresService } from '../../services/validadores.service';
 })
 export class ReactiveComponent implements OnInit {
 
-  forma!: FormGroup;
+  public forma!: FormGroup;
 
   constructor( private fb: FormBuilder,
                private validadores: ValidadoresService) {
                 
-    this.crearFormulario()
+    this.crearFormulario();
+    this.crearListeners();
    }
 
   ngOnInit(): void {
@@ -37,25 +39,41 @@ export class ReactiveComponent implements OnInit {
   ciudad( input: string ) {
     return this.forma.get(`${input}.ciudad`)?.invalid && this.forma.get(`${input}.ciudad`)?.touched
   }
-  
 
+  
+  get pass2NoValido(){
+    const pass1 = this.forma.get('pass1').value;
+    const pass2 = this.forma.get('pass2').value;
+
+    return ( pass1 === pass2 )? false : true
+  }
+  
   //sintaxis del objeto propiedad: ['valor por defecto','validadores sincronos',validadores asincronos]
                                          // sintaxis [validadores sincronos con 2 o mas validaciones , ...] 
   crearFormulario() {
     this.forma = this.fb.group({
-      nombre  : ['',   Validators.required],
-      apellido: ['',  [ Validators.required, this.validadores.noHerrera]],
-      correo  : ['', [ Validators.required, Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.com$') ] ],
+      nombre  : ['',   Validators.required ],
+      apellido: ['', [ Validators.required  , this.validadores.noHerrera]],
+      correo  : ['', [ Validators.required  , Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.com$') ] ],
+      usuario : ['',   Validators.required , this.validadores.existeUsuario],
+      pass1   : ['', [ Validators.required  , Validators.minLength(8)] ],
+      pass2   : ['',   Validators.required ],
       //group aninado en el formulario
       direccion: this.fb.group({
         provincia  : ['', Validators.required],
         ciudad     : ['', Validators.required]
       }),
       //array dentro del formulario
-      pasatiempos: this.fb.array([
+      pasatiempos: this.fb.array([])
+    }, {
+      Validators: [this.validadores.passwordsIguales('pass1','pass2'),this.validadores.validatePassword]
+    });
+  }
 
-      ])
-    }) 
+  crearListeners(){
+    this.forma.valueChanges.subscribe( valor => { console.log( valor )});
+    this.forma.statusChanges.subscribe( status => { console.log( {status} )});
+    this.forma.get( 'nombre' ).valueChanges.subscribe(console.log)
   }
 
   agregarPasatiempo(){
